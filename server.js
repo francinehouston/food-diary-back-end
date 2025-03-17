f1b992a5216305a5ef218eff6b0c7468c6beb052
“Francine” <francinehouston@gmail.com>
2025-03-16 21:47:46 -0400

    Cors update

--- server.js
+++ server.js
@@ -25,7 +25,16 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors({  origin: 'http://localhost:5173', credentials:true}));
+const corsOptions = {
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(logger('dev'));
 
