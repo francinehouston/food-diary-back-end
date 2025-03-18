520ec6fed3e9f9c6631625ea7740fc6515524245
“Francine” <francinehouston@gmail.com>
2025-03-18 06:30:18 -0400

    update cors

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const logger = require('morgan');
 const axios = require('axios');
 
 
-app.use(cors({origin: "https://food-diary-api-app.netlify.app"}));
+app.use(cors({origin: "https://food-diary-api-app.netlify.app",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true // If using cookies/auth headers
+}));
 app.use(express.json());
 app.use(logger('dev'));
 
